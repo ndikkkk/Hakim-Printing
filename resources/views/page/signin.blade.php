238826c83f39290573cfa5fed906ce92537c944e
Andhikaalvn <andhi618@gmail.com>
2026-04-29 10:52:16 +0700

    style: mbenahi layout signin & signup serta perbaikan rute logout

--- resources/views/page/signin.blade.php
+++ resources/views/page/signin.blade.php
@@ -1,68 +1,49 @@
 <!DOCTYPE html>
 <html>
-
 <head>
-    <title>Sign Up</title>
+    <title>Sign In</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <link rel="stylesheet" href="{{ asset('css/style-signup.css') }}">
 </head>
-
 <body>
 
     <div class="container">
-
         <div class="left">
             <img src="{{ asset('images/img2.jpeg') }}">
         </div>
 
-        {{-- RIGHT FORM --}}
         <div class="right">
             <h2>Sign In</h2>
 
             <form action="{{ route('signin.process') }}" method="POST">
                 @csrf
-                <div class="form-group">
-                    <label>Email</label>
-                    <div class="input-box">
-                        <input type="email" name="email" value="{{ old('email') }}" required>
-                    </div>
-                    @error('email')
-                        <span style="color:red; font-size:12px;">{{ $message }}</span>
-                    @enderror
-                </div>
 
-                <div class="form-group">
-                    <label>Password</label>
-                    <div class="input-box">
-                        <input type="password" name="password" required>
-                    </div>
+                <label>Email</label>
+                <div class="input-group">
+                    <img src="{{ asset('images/email.png') }}" class="icon">
+                    <input type="email" name="email" value="{{ old('email') }}" required>
                 </div>
+                @error('email')
+                    <span style="color:#b0435e; font-size:12px;">{{ $message }}</span>
+                @enderror
 
-                <div class="btn">
-                    <button type="submit">Sign In</button>
+                <label>Password</label>
+                <div class="input-group">
+                    <img src="{{ asset('images/password.png') }}" class="icon">
+                    <input type="password" name="password" required>
                 </div>
+                @error('password')
+                    <span style="color:#b0435e; font-size:12px;">{{ $message }}</span>
+                @enderror
+
+                <button type="submit">Sign In</button>
 
                 <p class="login">
                     Belum memiliki akun? <a href="{{ route('signup') }}">Sign Up</a>
                 </p>
             </form>
         </div>
-
     </div>
 
-    <script>
-        function handleSignup(e) {
-            e.preventDefault();
-
-            const email = document.querySelector('[name="email"]').value;
-
-            alert("Login berhasil: " + email);
-
-            window.location.href = "/signin";
-        }
-    </script>
-
 </body>
-
 </html>
--- resources/views/page/signup.blade.php
+++ resources/views/page/signup.blade.php
@@ -1,85 +1,64 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <title>Sign Up</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <link rel="stylesheet" href="{{ asset('css/style-signup.css') }}">
 </head>
-
 <body>
 
     <div class="container">
-
-        {{-- LEFT IMAGE --}}
         <div class="left">
             <img src="{{ asset('images/img2.jpeg') }}">
         </div>
 
-        {{-- RIGHT FORM --}}
         <div class="right">
             <h2>Sign Up</h2>
 
             <form action="{{ route('signup.process') }}" method="POST">
                 @csrf
-                <div class="form-group">
-                    <label>Nama Lengkap</label>
-                    <div class="input-box">
-                        <input type="text" name="name" value="{{ old('name') }}" required>
-                    </div>
+
+                <label>Nama Lengkap</label>
+                <div class="input-group">
+                    <img src="{{ asset('images/user-icon.png') }}" class="icon">
+                    <input type="text" name="name" value="{{ old('name') }}" required>
+                </div>
+                @error('name')
+                    <span style="color:#b0435e; font-size:12px;">{{ $message }}</span>
+                @enderror
+
+                <label>Email</label>
+                <div class="input-group">
+                    <img src="{{ asset('images/email.png') }}" class="icon">
+                    <input type="email" name="email" value="{{ old('email') }}" required>
+                </div>
+                @error('email')
+                    <span style="color:#b0435e; font-size:12px;">{{ $message }}</span>
+                @enderror
+
+                <label>Password</label>
+                <div class="input-group">
+                    <img src="{{ asset('images/password.png') }}" class="icon">
+                    <input type="password" name="password" required>
+                </div>
+                @error('password')
+                    <span style="color:#b0435e; font-size:12px;">{{ $message }}</span>
+                @enderror
+
+                <label>Konfirmasi Password</label>
+                <div class="input-group">
+                    <img src="{{ asset('images/password.png') }}" class="icon">
+                    <input type="password" name="password_confirmation" required>
                 </div>
 
-                <div class="form-group">
-                    <label>Email</label>
-                    <div class="input-box">
-                        <input type="email" name="email" value="{{ old('email') }}" required>
-                    </div>
-                    @error('email')
-                        <span style="color:red; font-size:12px;">{{ $message }}</span>
-                    @enderror
-                </div>
-
-                <div class="form-group">
-                    <label>Password</label>
-                    <div class="input-box">
-                        <input type="password" name="password" required>
-                    </div>
-                    @error('password')
-                        <span style="color:red; font-size:12px;">{{ $message }}</span>
-                    @enderror
-                </div>
-
-                <div class="form-group">
-                    <label>Konfirmasi Password</label>
-                    <div class="input-box">
-                        <input type="password" name="password_confirmation" required>
-                    </div>
-                </div>
-
-                <div class="btn">
-                    <button type="submit">Sign Up</button>
-                </div>
+                <button type="submit">Sign Up</button>
 
                 <p class="login">
                     Sudah memiliki akun? <a href="{{ route('signin') }}">Sign In</a>
                 </p>
             </form>
-
         </div>
-
-        <script>
-            function handleSignup(e) {
-                e.preventDefault();
-
-                const name = document.querySelector('[name="name"]').value;
-
-                alert("Akun berhasil dibuat, silakan login " + name);
-
-                window.location.href = "/signin1";
-            }
-        </script>
+    </div>
 
 </body>
-
 </html>
